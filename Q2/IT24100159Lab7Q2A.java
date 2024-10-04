980baf5c9c2a730370e4fe3cf9ad1f4a152ebca6
IT24100159 <it24100159@my.sliit.lk>
2024-10-04 11:08:54 +0530

    Add IT24100159Lab7Q2A.java to Q2

---
+++ Q2/IT24100159Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT24100159Lab7Q2A {
+    public static void main(String[] args){
+        int x,y;
+        for(x = 1;x <= 4;x++)
+        {
+            for(y = 1;y <= 5;y++)
+            {
+               System.out.print("$ ");
+            }
+            System.out.println();
+        }
+    }
+}
